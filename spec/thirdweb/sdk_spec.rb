d552400e1b5f7eebfc34d830576a9eef675a622a
Andres Yebra <andres.yebracervantes@gmail.com>
2023-03-31 21:52:38 -0700

    feat: initial commit

---
+++ spec/thirdweb/sdk_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Thirdweb::Sdk do
+  it "has a version number" do
+    expect(Thirdweb::Sdk::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
